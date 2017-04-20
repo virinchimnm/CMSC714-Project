cc15f1101c93b629982b89529b5bfd915df6e2b3
Somay Jain <somay.jain@gmail.com>
2017-04-20 00:35:47 -0400

    Changes to the run script

--- codes/run.sh
+++ codes/run.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
-### transform to zero_based_indexing of node ids
-#g++ scale_graph_to_zero.cpp -o scale_graph_to_zero
-#./scale_graph_to_zero -g ./data/in.txt > ./data/transformed.txt
+#######
+### Usage:
+### ./run.sh input_graph_downloaded num_part output_file_desired_BFS
 
 ### transform to zero_based_indexing of node ids
-#g++ output_to_gpmetis.cpp -o output_to_gpmetis
-#./output_to_gpmetis -g ./data/scaled.txt > ./data/gp_format.txt
+echo "Transforming to 0-based indexing"
+g++ scale_graph_to_zero.cpp -o scale_graph_to_zero
+./scale_graph_to_zero -g $1 > scaled.out
 
-### gpmetis usage to get
-#gpmetis path-to-input-graph no-of-partitions
+### transform to one_based_indexing of node ids
+g++ output_to_gpmetis.cpp -o output_to_gpmetis
+./output_to_gpmetis -g scaled.out > gp.out
+
+### gpmetis usage to get number of partitions
+gpmetis gp.out $2
 
 ### transform the output as needed by bfs
-#g++ output_to_graph_algo.cpp -o output_to_graph_algo
-#./output_to_graph_algo -g ./data/transformed.txt -p ./data/gp_format.txt.part.2
+g++ output_to_graph_algo.cpp -o output_to_graph_algo
+./output_to_graph_algo -g scaled.out -p gp.out.part.$2 > $3
+
+echo "Writing the output to file $3"
