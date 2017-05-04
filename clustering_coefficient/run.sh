ef8a6aef2cd71a398d1745284fdedbfa8d24d6ae
Virinchi <virinchimnm@gmail.com>
2017-05-03 21:34:32 -0400

    After removing unnecessary headers

--- clustering_coefficient/run.sh
+++ clustering_coefficient/run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-g++ serial_cc.cpp -o ./serial_cc  -std=c++0x 
-./serial_cc -g ./input/sample.txt
+#g++ serial_cc.cpp -o ./serial_cc  -std=c++0x 
+#./serial_cc -g ./input/sample.txt
 
 
+mpic++ parallel_cc.cpp -o ./parallel_cc  -std=c++0x 
+mpirun -n 2 ./parallel_cc -g ./input/try.txt
